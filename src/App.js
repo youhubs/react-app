ff97b6d1fccd7b41b2698741def9666a7e72617c
Hu Tiger <huxy99@yahoo.com>
2020-07-21 14:47:02 -0500

    lift state up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 import "./App.css";
 
-function App() {
-  return (
-    <React-frameElement>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React-frameElement>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    console.log("Event handler called.", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React-frameElement>
+        <NavBar />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
+        </main>
+      </React-frameElement>
+    );
+  }
 }
 
 export default App;
