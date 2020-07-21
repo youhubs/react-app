aaf27f1348613d0f7854c01c147180a129c5cecb
Hu Tiger <huxy99@yahoo.com>
2020-07-20 21:30:16 -0500

    set style attribute - 2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,15 +5,10 @@ class Counter extends Component {
     count: 0,
   };
 
-  styles = {
-    fontSize: 10,
-    fontWeight: "bold",
-  };
-
   render() {
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={{ fontSize: 10 }} className="badge badge-primary m-2">
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
