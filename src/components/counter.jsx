e1f67a4d3c60f4f9e22240dce3b45c1e5bd9a830
Hu Tiger <huxy99@yahoo.com>
2020-07-21 08:57:36 -0500

    add children to component by props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,7 +13,7 @@ class Counter extends Component {
     console.log("props", this.props);
     return (
       <React.Fragment>
-        {this.props.children}
+        <h4>Counter #{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
