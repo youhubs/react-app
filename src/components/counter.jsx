5535e95aafd0f93022c5f7e7efd9903cdc2bf121
Hu Tiger <huxy99@yahoo.com>
2020-07-20 22:01:31 -0500

    conditional rendering - &&

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react"; // imrc
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
 
   renderTags() {
@@ -22,7 +22,10 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <div>{this.renderTags()}</div>
+        <div>
+          {this.state.tags.length === 0 && "Please create a new tag!"}
+          {this.renderTags()}
+        </div>
       </React.Fragment>
     );
   }
