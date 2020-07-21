e36ac6e482910e203618e5f81fa8c55681a622ee
Hu Tiger <huxy99@yahoo.com>
2020-07-20 22:44:05 -0500

    pass event arguments - wrapper

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,9 +5,13 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement = () => {
-    console.log("increment clicked.", this);
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
+  };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
   };
 
   render() {
@@ -15,7 +19,7 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={this.doHandleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
