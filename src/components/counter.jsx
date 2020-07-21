4a2f5a624957ee188b696358e0360e1e76c9a495
Hu Tiger <huxy99@yahoo.com>
2020-07-20 21:19:05 -0500

    setting attributes: img example

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,11 +3,13 @@ import React, { Component } from "react"; // imrc
 class Counter extends Component {
   state = {
     count: 0,
+    imageUrl: "https://picsum.photos/50",
   };
 
   render() {
     return (
       <React.Fragment>
+        <img src={this.state.imageUrl} alt=""></img>
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
