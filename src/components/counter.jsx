1941ee12a758ee58e0e341f03f62755b720b35c9
Hu Tiger <huxy99@yahoo.com>
2020-07-20 21:28:49 -0500

    set style attributes - 1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,20 @@ import React, { Component } from "react"; // imrc
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/50",
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
   };
 
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.imageUrl} alt=""></img>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
